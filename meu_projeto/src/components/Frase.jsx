1c4c9dae358968e11d4a1d8cb0b1f63d92781f6c
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-12-16 11:49:39 -0300

    .

---
+++ meu_projeto/src/components/Frase.jsx
@@ -0,0 +1,9 @@
+function Frase(){
+    return (
+        <div>
+            <h1>Isso é uma frase</h1>
+        </div>
+    )
+}
+
+export default Frase
---
+++ meu_projeto/src/components/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import './Frase'
+import Frase from './Frase'
+
+function HelloWorld(){
+    return (
+        <div>
+            <p>Meu Primeiro componente
+            </p>
+            <Frase />
+        </div>
+    )
+}
+
+export default HelloWorld
